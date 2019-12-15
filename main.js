25a57dce2a6477596881c1ed78487e4d0fe8feff
AndreHofer <andreluizhofer@gmail.com>
2019-12-15 00:48:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,180 +1,171 @@
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/build/three.module.js';
 
-import Stats from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/libs/stats.module.js';
+var bins_face_url = "./fundo.jpg";
+var emilio_face_url = "./fundo.jpg";
+var marco_face_url = "./fundo.jpg";
+var dota2_url = "./fundo.jpg";
+var pill_icon = "./fundo.jpg";
+var bueno_icon = "./fundo.jpg";
+var fernando_face_url = "./fundo.jpg";
 
-import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/controls/OrbitControls.js';
+var scene, camera, renderer;
+var rigth_cube, left_cube, top_cube, bottom_cube;
+var velocity = 0.04;
 
-import { GUI } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/libs/dat.gui.module.js';
+var player = {
+  id: 'null',
+  shape: new THREE.Mesh()
+}
 
-import { GLTFLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/loaders/GLTFLoader.js';
+var texture = new THREE.TextureLoader().load(emilio_face_url);
+var material = new THREE.MeshBasicMaterial();
+material.map = texture;
 
-var container, stats, clock, gui, mixer, actions, activeAction;
-var camera, scene, renderer, model, controls;
+const init = () => {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 3;
 
-var xSpeed = 10;
-var zSpeed = 10;
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setClearColor("#000");
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+  window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+  });
+
+  const light = new THREE.PointLight(0xFFFFFF, 1, 1000);
+  light.position.set(0, 0, 0);
+  scene.add(light);
+
+  initializeBorders();
+  initializePlayer();
+
+  document.addEventListener('keydown', handleKeydown, false);
+  render();
+}
+
+const setPlayerId = (id) => {
+  player.id = id;
+}
+
+const initializeBorders = () => {
+  let geometry = new THREE.BoxGeometry(1.5, 5, 1);
+  let material = new THREE.MeshBasicMaterial({ depthWrite: false, depthTest: false })
+
+  rigth_cube = new THREE.Mesh(geometry, material);
+  rigth_cube.position.set(4.5, 0, 0.5);
+  left_cube = new THREE.Mesh(geometry, material);
+  left_cube.position.set(-4.5, 0, 0.5);
+
+  geometry = new THREE.BoxGeometry(9, 1.5, 1);
+  top_cube = new THREE.Mesh(geometry, material);
+  top_cube.position.set(0, 2.5, 0.5);
+  bottom_cube = new THREE.Mesh(geometry, material);
+  bottom_cube.position.set(0, -2.5, 0.5);
+
+  geometry = new THREE.BoxGeometry(8.5, 4.5, 0.00001);
+  material = new THREE.MeshBasicMaterial({ depthWrite: false, depthTest: false })
+  const cube = new THREE.Mesh(geometry, material);
+  cube.position.set(0, 0, 0);
+
+  const edges = new THREE.EdgesGeometry(cube.geometry);
+  const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
+  scene.add(line);
+}
+
+const handleChangeTeacherTexture = (teacher_id) => {
+  const accepted_textures = {
+    Bins() { return bins_face_url },
+    Emilio() { return emilio_face_url },
+    Marco() { return marco_face_url },
+    Fernando() { return fernando_face_url }
+  }
+
+  if (teacher_id === player.id) return;
+
+  const teacherTexture = accepted_textures[teacher_id];
+
+  if (teacherTexture) {
+    setPlayerId(teacher_id)
+    const url = teacherTexture()
+    const texture = new THREE.TextureLoader().load(url);
+    material.map = texture;
+  }
+}
+
+const initializePlayer = () => {
+  const geometry = new THREE.CircleGeometry(0.25, 32);
+  player.shape = new THREE.Mesh(geometry, material);
+  player.shape.rotateZ(Math.random() * (0, 2) * Math.PI);
+  scene.add(player.shape);
+}
+
+const isCollider = (teste_cube) => {
+  const circle_BB = new THREE.Box3().setFromObject(player.shape);
+  const cube_BB = new THREE.Box3().setFromObject(teste_cube);
+  const is_collide = cube_BB.containsBox(circle_BB);
+
+  return is_collide;
+}
+
+const movePlayer = (comand) => {
+  const accepted_moves = {
+    ArrowUp() {
+      if (!isCollider(top_cube)) player.shape.rotateZ(0.05);
+    },
+    ArrowDown() {
+      if (!isCollider(bottom_cube)) player.shape.rotateZ(-0.05);
+    },
+    ArrowLeft() {
+      if (!isCollider(left_cube)) player.shape.rotateZ(-0.05);
+    },
+    ArrowRight() {
+      if (!isCollider(rigth_cube)) player.shape.rotateZ(0.05);
+    }
+  };
+
+  // agora quando eu precisar colocar mais uma tecla, é só colocar mais um objeto e 
+  // implementar a regra de negócio, não preciso verificar mais em lugar nenhum se tem 
+  // ou não a tecla e tudo mais. Isso deixa o código com a menor fricção possível
+
+  const key_press = comand.key;
+  const moveFunction = accepted_moves[key_press];
+
+  if (moveFunction) moveFunction();
+}
+
+const handleKeydown = (event) => {
+  movePlayer(event);
+  // console.log('Tecla pressionada: ', event.key)
+}
+
+const changeRotate = (number) => {
+  const rotate = ((player.shape.rotation.z / Math.PI) + number) * -1 * Math.PI;
+  player.shape.rotation.z = rotate;
+}
+
+const render = () => {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+
+  if (isCollider(top_cube)) {
+    changeRotate(0);
+    handleChangeTeacherTexture('Emilio')
+  } else if (isCollider(bottom_cube)) {
+    changeRotate(0);
+    handleChangeTeacherTexture('Bins')
+  } else if (isCollider(left_cube)) {
+    changeRotate(1);
+    handleChangeTeacherTexture('Marco')
+  } else if (isCollider(rigth_cube)) {
+    changeRotate(1);
+    handleChangeTeacherTexture('Fernando')
+  }
+  player.shape.translateX(velocity)
+}
 
 init();
-animate();
-
-function init() {
-
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
-
-    camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 0.25, 1000 );
-    camera.position.set( 0, 20, 30 );
-    camera.lookAt( new THREE.Vector3( 0, 2, 0) );
-
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x000000 );
-    scene.fog = new THREE.Fog( 0x000000, 2, 1000 );
-
-    clock = new THREE.Clock();
-
-    // lights
-    var light = new THREE.HemisphereLight( 0x000000, 0x004400 );
-    light.position.set( 0, 10, 0 );
-    scene.add( light );
-    light = new THREE.DirectionalLight( 0xffffff );
-    light.position.set( 0, 20, 10 );
-    scene.add( light );
-
-    // ground
-    var gt = new THREE.TextureLoader().load("./fundo.jpg");
-    var gg = new THREE.PlaneBufferGeometry(1000,1000);
-    var gm = new THREE.MeshPhongMaterial({ color: 0xffffff, map: gt });
-
-    var ground = new THREE.Mesh(gg, gm);
-    ground.rotation.x = - Math.PI / 2;
-    ground.material.map.repeat.set(25, 25);
-    ground.material.map.wrapS = ground.material.map.wrapT = THREE.RepeatWrapping;
-    ground.receiveShadow = true;
-
-    scene.add(ground);
-
-    
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.gammaOutput = true;
-    renderer.gammaFactor = 2.2;
-    container.appendChild( renderer.domElement );
-
-    window.addEventListener('resize', onWindowResize, false );
-    window.addEventListener('keydown', onDocumentKeyDown, false );
-
-    stats = new Stats();
-    container.appendChild( stats.dom );
-
-    controls = new OrbitControls( camera, renderer.domElement );
-
-}
-
-
-
-
-function createGUI( model, animations ) {
-    gui = new GUI();
-    mixer = new THREE.AnimationMixer( model );
-    actions = {};
-
-    var clip = animations[0];
-    var action = mixer.clipAction( clip );
-    actions[ clip.name ] = action;
-
-    activeAction = actions[ 'Take 001' ];
-    activeAction.play();
-}
-
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-function onDocumentKeyDown(e) {
-    var keyCode = event.which;
-    if (e.code != 'Space'){
-        if (keyCode == 87) {
-            model.position.z += zSpeed;
-        } else if (keyCode == 83) {
-            model.position.z -= zSpeed;
-        } else if (keyCode == 65) {
-            model.position.x += xSpeed;
-        } else if (keyCode == 68) {
-            model.position.x -= xSpeed;
-        }
-    }else{
-        if (activeAction._clip.name == 'Take 001') {
-            activeAction.paused = true;
-            activeAction._clip.name = ''
-        }else{
-            activeAction._clip.name = 'Take 001'
-            activeAction.paused = false;
-        }
-    }
-};
-
-function animate() {
-    var dt = clock.getDelta();
-    if ( mixer ) mixer.update( dt );
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-    stats.update();
-    // required if controls.enableDamping or controls.autoRotate are set to true
-    controls.update();
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-function main() {
-  const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer({canvas});
-
-  const fov = 75;
-  const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 5;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 2;
-
-  const scene = new THREE.Scene();
-
-  const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
-  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-  const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
-
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  renderer.render(scene, camera);
-}
-
-main();
-*/
